77ffd6f993b66adfe689363b4709343bd1e70ffe
T04435 <me@t04435.dev>
2022-11-04 05:38:27 -0400

    Update .install.sh

--- .install.sh
+++ .install.sh
@@ -22,25 +22,13 @@ fi
 # install packages
 brew install \
     bash-completion \
-    bazelisk \
     git-gui \
     maven \
     nvm \
     python@3.7 \
     tmux \
     yarn \
-    deno \
     tmux \
-
-# deno
-deno upgrade --version 1.8.3
-deno install --allow-read --allow-write --allow-run --unstable -f --name dnit https://deno.land/x/dnit@dnit-v1.12.1/main.ts
-
-# install sdkman - OpenJDK8
-curl -s "https://get.sdkman.io" | bash
-source "$HOME/.sdkman/bin/sdkman-init.sh"
-sdk version
-sdk install java 11.0.11.j9-adpt
 
 # Install python packages
 sudo pip3 install doit pystache
